ff1112cd889846a44579fe7c392cc80a0a4f1d9b
CM Student <cmstudent@UHWO-ACM-16.local>
2018-02-21 15:34:06 -1000

    Done! Learned about different loops.

--- SwiftPlaygroundFour.playground/Contents.swift
+++ SwiftPlaygroundFour.playground/Contents.swift
@@ -7,3 +7,29 @@ print(str)
 
 //Beginning Swift Playgrounds Four
 
+var fruitNames = ["Kiwi", "Apples", "Watermelon"]
+for name in fruitNames {
+    print("I like to eat " + name)
+}
+
+//for number in 1...5 {
+   // print(number)
+//}
+
+var sum = 0
+for number in 1...5
+{
+    sum += number
+    print(sum)
+}
+
+var i = 1
+while i <= 10
+{
+    //we want to print i
+    print(i)
+    //we want to add + 1 to i for each iteration through the loop
+    i = i + 1
+}
+
+//finished Swift Playgrounds Four
